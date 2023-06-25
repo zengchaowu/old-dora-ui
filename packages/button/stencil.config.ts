208152c5881e140511fe2dff6122f35b1b9cb6d8
zengcw <zengcw@300624.cn>
2023-06-25 15:40:57 +0800

    feat(button): init with stencil

---
+++ packages/button/stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'button',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
